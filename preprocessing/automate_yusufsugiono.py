90dc03bc1a12d4f1291363c44c53c53c55561aff
Yusuf Sugiono <ysf.sugiono@gmail.com>
2026-03-11 16:48:09 +0700

    improve automation code

--- preprocessing/automate_yusufsugiono.py
+++ preprocessing/automate_yusufsugiono.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.impute import SimpleImputer
@@ -34,35 +35,46 @@ def preprocess_crop_data(
         stratify=y
     )
 
-    # Buat pipeline untuk preprocess
+    # Pipeline
     numeric_pipeline = Pipeline([
         ("imputer", SimpleImputer(strategy="mean")),
         ("scaler", StandardScaler())
     ])
+
     X_train = numeric_pipeline.fit_transform(X_train)
     X_test = numeric_pipeline.transform(X_test)
 
-    # Simpan hasil preprocess
     X_train = pd.DataFrame(X_train, columns=X.columns)
     X_test = pd.DataFrame(X_test, columns=X.columns)
     X_train[target_column] = y_train
     X_test[target_column] = y_test
+    Path(train_output_path).parent.mkdir(parents=True, exist_ok=True)
+
+    # Simpan dataset hasil preprocess beserta artefaknya
     X_train.to_csv(train_output_path, index=False)
     X_test.to_csv(test_output_path, index=False)
-
-    # Simpan artifacts
     dump(numeric_pipeline, preprocessor_path)
     dump(label_encoder, encoder_path)
+
+    # Simpan nama fitur
+    pd.Series(X.columns).to_csv(
+        Path(preprocessor_path).parent / "feature_columns.csv",
+        index=False
+    )
 
     print("Train dataset saved to:", train_output_path)
     print("Test dataset saved to:", test_output_path)
 
+
 if __name__ == "__main__":
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
     preprocess_crop_data(
-        dataset_path="CropRecommendation_raw.csv",
+        dataset_path=BASE_DIR / "CropRecommendation_raw.csv",
         target_column="label",
-        train_output_path="preprocessing/CropRecommendation_preprocessing/train_preprocessed.csv",
-        test_output_path="preprocessing/CropRecommendation_preprocessing/test_preprocessed.csv",
-        preprocessor_path="preprocessing/CropRecommendation_preprocessing/preprocessor.joblib",
-        encoder_path="preprocessing/CropRecommendation_preprocessing/label_encoder.joblib"
+        train_output_path=BASE_DIR / "preprocessing/CropRecommendation_preprocessing/train_preprocessed.csv",
+        test_output_path=BASE_DIR / "preprocessing/CropRecommendation_preprocessing/test_preprocessed.csv",
+        preprocessor_path=BASE_DIR / "preprocessing/CropRecommendation_preprocessing/preprocessor.joblib",
+        encoder_path=BASE_DIR / "preprocessing/CropRecommendation_preprocessing/label_encoder.joblib"
     )
